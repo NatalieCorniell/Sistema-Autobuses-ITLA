94ed738627403a33bfd07b1e7567a5db65f4a8c0
Natalie Georgia Corniell Rojas <nataliecorniell@MacBook-Pro-de-Natalie.local>
2020-02-27 12:05:42 -0400

    GitIgnore

---
+++ Autobuses-Itla/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutobusesItla
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
